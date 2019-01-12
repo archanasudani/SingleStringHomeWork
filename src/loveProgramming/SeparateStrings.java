32d8526928fd50a022e34660a090aa95e6be0b6d
archana sudani <archanasudani@gmail.com>
2019-01-12 19:11:01 +0000

    My First Commit

---
+++ src/loveProgramming/SeparateStrings.java
@@ -0,0 +1,22 @@
+package loveProgramming;
+
+public class SeparateStrings {
+    public static void main(String[] args) {
+
+        String s = "";
+        String s1 = "The ";
+        String s2 = "String ";
+        String s3 = "Concatenation ";
+        String s4 = "works.";
+        String s5 = "";
+
+        System.out.println(s5);
+        System.out.println(s1 + s2 + s3 + s4);
+        s5 = s5.concat(s1 + s2 + s3 + s4);
+        System.out.println();
+    }
+
+}
+
+
+
---
+++ src/loveProgramming/SingleLineLoopStingConcatenation.java
@@ -0,0 +1,15 @@
+package loveProgramming;
+
+public class SingleLineLoopStingConcatenation {
+    public static void main(String[] args) {
+
+        String s = " ";
+            for (int i = 20; i > 0; i--) {
+            System.out.print(s + "  " + i);
+        }
+
+    }
+
+}
+
+
